a672e8f9887dc3795152411a24d0d2ee0a123e63
ubugeeei <ubuge1122@gmail.com>
2026-06-12 17:39:23 +0900

    feat(eslint-comments): expose directive scan apis

--- npm/eslint-comments/api.js
+++ npm/eslint-comments/api.js
@@ -27,11 +27,104 @@ function normalizeComments(comments) {
   });
 }
 
+function normalizeStrings(name, values) {
+  if (!Array.isArray(values) || values.some((value) => typeof value !== 'string')) {
+    throw new TypeError(`${name} must be an array of strings.`);
+  }
+
+  return values;
+}
+
+function normalizePosition(position) {
+  if (!position || typeof position.line !== 'number' || typeof position.column !== 'number') {
+    throw new TypeError('position must have numeric line and column fields.');
+  }
+
+  return {
+    line: position.line >>> 0,
+    column: position.column | 0,
+  };
+}
+
+function normalizeProblems(problems = []) {
+  if (!Array.isArray(problems)) {
+    throw new TypeError('problems must be an array.');
+  }
+
+  return problems.map((problem) => {
+    if (!problem || (problem.ruleId != null && typeof problem.ruleId !== 'string')) {
+      throw new TypeError('each problem must have a nullable string ruleId.');
+    }
+
+    const line = problem.line ?? problem.loc?.start?.line;
+    const column = problem.column ?? problem.loc?.start?.column;
+    if (typeof line !== 'number' || typeof column !== 'number') {
+      throw new TypeError('each problem must have numeric line and column fields.');
+    }
+
+    return {
+      ruleId: problem.ruleId == null ? null : problem.ruleId,
+      line: line >>> 0,
+      column: column | 0,
+    };
+  });
+}
+
+function scanDisableEnablePair(comments, allowWholeFile = false, firstTokenStart = null) {
+  return native.scanDisableEnablePair(
+    normalizeComments(comments),
+    !!allowWholeFile,
+    firstTokenStart == null ? null : normalizePosition(firstTokenStart),
+  );
+}
+
+function scanNoAggregatingEnable(comments) {
+  return native.scanNoAggregatingEnable(normalizeComments(comments));
+}
+
+function scanNoDuplicateDisable(comments) {
+  return native.scanNoDuplicateDisable(normalizeComments(comments));
+}
+
+function scanNoRestrictedDisable(comments, patterns = []) {
+  return native.scanNoRestrictedDisable(
+    normalizeComments(comments),
+    normalizeStrings('patterns', patterns),
+  );
+}
+
 function scanNoUnlimitedDisable(comments) {
   return native.scanNoUnlimitedDisable(normalizeComments(comments));
 }
 
+function scanNoUnusedDisable(comments, problems = []) {
+  return native.scanNoUnusedDisable(normalizeComments(comments), normalizeProblems(problems));
+}
+
+function scanNoUnusedEnable(comments) {
+  return native.scanNoUnusedEnable(normalizeComments(comments));
+}
+
+function scanNoUse(comments, allow = []) {
+  return native.scanNoUse(normalizeComments(comments), normalizeStrings('allow', allow));
+}
+
+function scanRequireDescription(comments, ignore = []) {
+  return native.scanRequireDescription(
+    normalizeComments(comments),
+    normalizeStrings('ignore', ignore),
+  );
+}
+
 module.exports = {
+  scanDisableEnablePair,
+  scanNoAggregatingEnable,
+  scanNoDuplicateDisable,
+  scanNoRestrictedDisable,
   scanNoUnlimitedDisable,
+  scanNoUnusedDisable,
+  scanNoUnusedEnable,
+  scanNoUse,
+  scanRequireDescription,
 };
 module.exports.default = module.exports;
